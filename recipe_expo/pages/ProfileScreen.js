fb08e1c6cb838ca532d58cd8d8eeaab4eb9ee3b2
Kondreddy Balaji Sudarshan Reddy <kondreddy2007@gmail.com>
2026-06-14 10:27:29 +0530

    Completed My Recipe in profile section

--- recipe_expo/pages/ProfileScreen.js
+++ recipe_expo/pages/ProfileScreen.js
@@ -7,6 +7,7 @@ import {
   TouchableOpacity,
   StyleSheet,
   Dimensions,
+  FlatList,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
@@ -23,17 +24,18 @@ const ProfileScreen = () => {
   const { user, loading, handleLogout } = useAuth();
 
   const [userRecipes, setUserRecipes] = useState([]);
-  const [userRecipeCount,setUserRecipeCount] = useState(0)
+  const [userRecipeCount, setUserRecipeCount] = useState(0);
   const [loadingRecipes, setLoadingRecipes] = useState(true);
 
   const fetchUserRecipes = async () => {
     try {
+      setLoadingRecipes(true);
       const response = await getMyRecipes();
       if (response && response.data && response.data.recipes) {
         setUserRecipes(response.data.recipes);
-        setUserRecipeCount(response.data.count)
+        setUserRecipeCount(response.data.count);
       }
-      console.log(response.data.recipes)
+      console.log(response.data.recipes);
     } catch (error) {
       console.log("Error loading recipes:", error);
     } finally {
@@ -140,15 +142,75 @@ const ProfileScreen = () => {
             </Text>
           </TouchableOpacity>
         </View>
-        <>
-          <View style={styles.yourRecipeContainer}>
-            <Text style={styles.headingTxt}>Your Recipes - {userRecipeCount}</Text>
+        {loadingRecipes ? (
+          <View style={{ marginTop: height * 0.02 }}>
+            <ActivityIndicator />
           </View>
+        ) : (
+          <>
+            <View style={styles.yourRecipeContainer}>
+              <Text style={styles.headingTxt}>
+                Your Recipes - {userRecipeCount}
+              </Text>
+            </View>
 
-          <View>
-             
-          </View>
-        </>
+            <View style={styles.profileRecipeContainer}>
+              {userRecipes.slice(0, 5).map((recipe) => (
+                <View key={recipe.id} style={styles.profileRecipeCard}>
+                  <Image
+                    source={{ uri: recipe.image }}
+                    style={styles.recipeImage}
+                  />
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <View style={styles.recipeContent}>
+                      <Text style={styles.recipeTitle} numberOfLines={1}>
+                        {recipe.title}
+                      </Text>
+                      <Text style={styles.recipeCuisine}>{recipe.cuisine}</Text>
+                      <Text
+                        style={[
+                          styles.badge,
+                          recipe.foodType === "VEG"
+                            ? styles.vegBadge
+                            : styles.nonVegBadge,
+                        ]}
+                      >
+                        {recipe.foodType}
+                      </Text>
+                    </View>
+                    <View
+                      style={{ flex: "1", marginRight: "5%", marginTop: "5%" }}
+                    >
+                      <Feather name="heart" size={15} color="red" />
+                      <Text style={{ fontWeight: "800", color: "red" }}>
+                        13
+                      </Text>
+                    </View>
+                  </View>
+                </View>
+              ))}
+            </View>
+            <TouchableOpacity
+              style={[styles.editButton, styles.fullWidthButton]}
+              onPress={() => navigation.navigate("EditProfile")}
+            >
+              <Text style={styles.btnText} numberOfLines={1}>
+                View All
+              </Text>
+              <Feather
+                name="arrow-right"
+                size={width * 0.04}
+                color="#fff"
+                style={{ marginRight: 6 }}
+              />
+            </TouchableOpacity>
+          </>
+        )}
       </View>
     </ScrollView>
   );
@@ -301,6 +363,73 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "#6b6b6b",
     textAlign: "left",
+  },
+
+  profileRecipeCard: {
+    width: "48%",
+    backgroundColor: "#fff",
+    borderRadius: 15,
+    marginBottom: 15,
+    overflow: "hidden",
+
+    elevation: 3,
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 4,
+  },
+  profileRecipeContainer: {
+    width: "90%",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    marginTop: 15,
+  },
+  recipeImage: {
+    width: "100%",
+    height: width * 0.25,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+  },
+  recipeContent: {
+    padding: 12,
+  },
+  recipeTitle: {
+    fontSize: 15,
+    fontWeight: "700",
+    color: "#222",
+  },
+  recipeCuisine: {
+    marginTop: 4,
+    fontSize: 12,
+    color: "#888",
+    textTransform: "uppercase",
+  },
+  badge: {
+    alignSelf: "flex-start",
+    backgroundColor: "#FFF0E0",
+    color: "#FF7A00",
+    paddingHorizontal: 8,
+    paddingVertical: 3,
+    borderRadius: 8,
+    fontSize: 11,
+    fontWeight: "600",
+    marginTop: 6,
+  },
+  vegBadge: {
+    backgroundColor: "#DCFCE7",
+    color: "#15803D",
+  },
+
+  nonVegBadge: {
+    backgroundColor: "#FFEDD5",
+    color: "#EA580C",
+  },
+  fullWidthButton: {
+    width: "90%",
   },
 });
 
